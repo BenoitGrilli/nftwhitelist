9f6799a742a9547bd8cc06c4a0829ba5de8dc86b
benwa06 <benwa06@hotmail.fr>
2023-02-21 15:36:59 +0100

    Reload page account/network/disconnected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import { ethers} from 'ethers';
 import './App.css';
 
+// problème à résoudre : MetaMask demande de se connecter même lorsqu'on est connecté.
+
+
 // Connecter MetaMask en LocalHost
 function App() {
 
@@ -13,18 +16,43 @@ function App() {
 
   useEffect(() => {
     getAccounts();
+    setLoader(false);
   }, [])
+
+  window.ethereum.addListener('connect', async(response) => {
+    getAccounts();
+    console.log('ok');
+  })
+
+// utilisateur change de compte => reload
+  window.ethereum.on('accountsChanged', () => {
+    window.location.reload();
+  })
+// utilisateur change de network => reload
+  window.ethereum.on('chainChanged', () => {
+    window.location.reload();
+  })
+// utilisateur se déconnecte de MetaMask => reload
+  window.ethereum.on('disconnect', () => {
+    window.location.reload();
+  })
 
   async function getAccounts() {
     if(typeof window.ethereum !== 'undefined') {
       let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      console.log(accounts);
+      setAccounts(accounts);
+
     }
   }
 
   return (
     <div className="App">
-      
+        {!loader && 
+          accounts.length > 0 ?
+          <p>You are connected with this account : {accounts[0]}</p>
+          :
+          <p>You are not connected with Metamask on this website.</p>
+        }
     </div>
   );
 }
